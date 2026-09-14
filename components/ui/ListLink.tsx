7d0317cbc887591a64a6fb068b8f21b2c9d710db
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-09-14 13:56:06 -0300

    feat: criando componente ListLink e adicionando estilo

---
+++ components/ui/ListLink.tsx
@@ -0,0 +1,10 @@
+
+type ListLinkT = {title: string, link?: string}
+
+export const ListLink = ({title, link}:ListLinkT) => {
+  return (
+    <li className="my-2 xl:my-0 xl:mx-6">
+        <a href={`#${link}`}>{title}</a>
+    </li>
+  )
+}
