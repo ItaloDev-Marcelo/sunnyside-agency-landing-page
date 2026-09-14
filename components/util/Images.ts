3e8b1affdd8c97c0ff7775f58d381c93c47b4f99
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-09-14 13:55:39 -0300

    feat: criando Images, função de exportação de images

---
+++ components/util/Images.ts
@@ -0,0 +1,58 @@
+import galleryConeD from '../../assets/images/desktop/image-gallery-cone.jpg'
+import galleryMilkbottlesD from '../../assets/images/desktop/image-gallery-milkbottles.jpg'
+import galleryOrangeD from '../../assets/images/desktop/image-gallery-orange.jpg'
+import gallerySugarcubesD from '../../assets/images/desktop/image-gallery-sugarcubes.jpg'
+import graphicD from '../../assets/images/desktop/image-graphic-design.jpg'
+import photographyD from '../../assets/images/desktop/image-photography.jpg'
+import standOutD from '../../assets/images/desktop/image-stand-out.jpg'
+import TransformD from '../../assets/images/desktop/image-transform.jpg'
+import galleryConeM from '../../assets/images/mobile/image-gallery-cone.jpg'
+import galleryMilkbottlesM from '../../assets/images/mobile/image-gallery-milkbottles.jpg'
+import galleryOrangeM from '../../assets/images/mobile/image-gallery-orange.jpg'
+import gallerySugarcubesM from  '../../assets/images/mobile/image-gallery-sugar-cubes.jpg'
+import graphicM from '../../assets/images/mobile/image-graphic-design.jpg'
+import photographyM from '../../assets/images/mobile/image-photography.jpg'
+import standOutM from '../../assets/images/mobile/image-stand-out.jpg'
+import TransformM from '../../assets/images/mobile/image-transform.jpg'
+import arrowDown from '../../assets/images/icon-arrow-down.svg'
+import facebook from '../../assets/images/icon-facebook.svg'
+import hamburger from '../../assets/images/icon-hamburger.svg'
+import instagram from '../../assets/images/icon-instagram.svg'
+import pintarest from '../../assets/images/icon-pinterest.svg'
+import twitter from '../../assets/images/icon-twitter.svg'
+import emily from '../../assets/images/image-emily.jpg'
+import jennie from '../../assets/images/image-jennie.jpg'
+import thomas from '../../assets/images/image-thomas.jpg'
+import logo from '../../assets/images/logo.svg'
+
+
+
+export {
+ galleryConeD, 
+ galleryMilkbottlesD, 
+ galleryOrangeD, 
+ gallerySugarcubesD,
+ graphicD, 
+ photographyD, 
+ standOutD, 
+ TransformD, 
+ galleryConeM, 
+ galleryMilkbottlesM, 
+ galleryOrangeM, 
+ gallerySugarcubesM, 
+ graphicM,  
+ photographyM, 
+ standOutM, 
+ TransformM,
+ arrowDown, 
+ facebook,
+ hamburger,
+ instagram,
+ pintarest,
+ twitter,
+ emily,
+ jennie,
+ thomas,
+ logo
+}
+
