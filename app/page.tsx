6bc0dd89ddcdaefdda6044484a342cbce16e22f3
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-09-16 14:49:39 -0300

    feat: criando Main e estilizando

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/components/layout/Hero";
+import { Main } from "@/components/layout/Main";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Main />
     </>
   );
 }
