14380d48c8f155f41a5a607ca6c68b0f970b605e
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-09-18 10:41:33 -0300

    feat: adicionando style in ListLink

--- components/ui/ListLink.tsx
+++ components/ui/ListLink.tsx
@@ -1,8 +1,8 @@
 import { ListLinkT } from '../../types/ListT'
 
-export const ListLink = ({title, link}:ListLinkT) => {
+export const ListLink = ({title, link, style}:ListLinkT) => {
   return (
-    <li className="my-2 xl:my-0 xl:mx-6">
+    <li className={`my-2 xl:my-0 xl:mx-6 cursor-pointer ${style}`}>
         <a href={`#${link}`}>{title}</a>
     </li>
   )
