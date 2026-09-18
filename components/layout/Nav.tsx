332304ba6aeb19a4f1e330560ae0ffbf179ffb39
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-09-18 10:37:01 -0300

    style: adicionando mais estilo e adicionando style em ListLink

--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -16,11 +16,11 @@ export const Nav = () => {
        <ul className={`${navbar ? 'flex absolute top-30 left-[6%] w-[330px] h-[250px] bg-white ' : 'hidden'} text-black xl:text-white xl:flex flex-col items-center justify-center xl:justify-start xl:flex-row gap-y-3 xl:gap-0`}>
         {
         listItems.map((item,index) => (
-          <ListLink title={item} key={index} />
+          <ListLink title={item} key={index} style='hover:text-green500 active:text-green500' />
         ))
        }
        <li className='my-2 xl:my-0 xl:ml-10'>
-        <button className='w-35 h-13 rounded-full hover:bg-yellow-400 hover:text-white text-black bg-white'>Contact</button>
+        <button className='w-34 h-10 rounded-full text-[14px] font-black font-fraunces uppercase active:bg-yellow-400 hover:bg-yellow-400 xl:hover:bg-[#6fcfff] xl:hover-text-white hover:text-white text-black bg-white'>Contact</button>
        </li>
        </ul>
     </nav>
